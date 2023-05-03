e4a505a032d7b571e821d9a3ef8ef072b8a41f47
projectgiorgibregadze <bregadzeg8@gmail.com>
2023-05-04 01:48:53 +0400

    login/registration v2

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # ITstep_fin_project
 Final project for ItStep
 
-Starting soon 
+# Extensions that i use 
+1.Prettier-code formatter
+2.jQuery Code Snippets
+3.Live Server
+
+# Code source
+1. არ ვიყენებ არავის კოდს
+2. თუ რაიმე ვერ გავიგე კვლავ ლექციებს ვავვლებ თვალს ან ვეძებ ინტერნეტში
+
+# Developer
+ გიორგი ბრეგაძე
--- login_register/register_login.js
+++ login_register/register_login.js
@@ -1,1 +1,23 @@
-// ჯერ ჯერობით ცარიელია რადგან დიზაინი არ დამისრულებია :)
+// ფუნქცია პაროლის აღდგენისა და მისი შეცვლისათვის
+let index_forgot = 0;
+$("#forgot").click(function () {
+  if (index_forgot == 0) {
+    $("#passplace,#br2,#br3").remove();
+    $("#Emailplace").prop("placeholder", "Email where we can send code");
+    $("#button").html("Get Code");
+    $("#forgot").html("Go back?");
+    index_forgot = 1;
+  } else {
+    $("#forgot").html("Forgot passowrd?");
+    $("#Emailplace").prop("placeholder", "Email");
+    $("#br1").after(
+      '<br id="br2"/>\n<input id="passplace"type="password"minlength="5"placeholder="Password"required/>\n<br id="br3"/>'
+    );
+    index_forgot = 0;
+  }
+});
+$("#button").click(function () {
+  $("#Emailplace").prop("placeholder", "Write code here");
+  $("#button").html("Submit");
+});
+// ჯერ ჯერობით არ დამიმთავრებია
