a1bb1b4ef9f061e72c4fe1256d0ee4e9711cfd09
projectgiorgibregadze <bregadzeg8@gmail.com>
2023-05-10 01:28:33 +0400

    start

--- login_register/register_login.js
+++ login_register/register_login.js
@@ -1,5 +1,5 @@
 // fixes needed
-
+var emailFound = false;
 let index_reg = 0;
 let index_code = 0;
 let forgot_mail = "";
@@ -11,6 +11,7 @@ $("#forgot").click(function () {
     $("#Emailplace").val("");
     $("#passplace").val("");
     index_reg = 0;
+    index_login = 0;
   } else if (index_code == 0) {
     $("#passplace,#br2,#br3").remove();
     $("#Emailplace").prop("placeholder", "Email where we can send code");
@@ -48,32 +49,67 @@ $("#button").click(function (event) {
     });
   } else if (index_reg == 1) {
     $.getJSON("data.json", function (data) {
-      $.each(data.users, function (index, user) {
-        if (user.mail === $("#Emailplace").val() && index_reg == 1) {
-          alert("This Email Is Already Used By Someone");
-        } else if (user.mail != $("#Emailplace").val()) {
-          $.getJSON("data.json", function (data) {
-            const newUser = {
-              mail: $("#Emailplace").val(),
-              password: $("#passplace").val(),
-            };
-            data.users.push(newUser);
-            $.ajax({
-              url: "data.json",
-              type: "PUT",
-              data: JSON.stringify(data),
-              contentType: "application/json",
-            });
-          });
+      var value = $("#Emailplace").val();
+      var length = value.length;
+      if (length != 0) {
+        $.each(data.users, function (index, user) {
+          if (user.mail == $("#Emailplace").val()) {
+            $("#errortext").html("This Email Has Been Already Used By Someone");
+            $("#Errordiv").removeClass("display");
+            setTimeout(function () {
+              $("#Errordiv").addClass("slide-out");
+              $("#Errordiv").removeClass("slide-in");
+              setTimeout(function () {
+                $("#Errordiv").removeClass("slide-out");
+                $("#Errordiv").addClass("display");
+              }, 500);
+            }, 2000);
+          }
+        });
+        if (!emailFound) {
+          if ($("#passplace").val() == $("#passplace2").val()) {
+            console.log(123);
+          } else {
+            $("#errortext").html("Passwords Are Not Same");
+            $("#Errordiv").removeClass("display");
+            setTimeout(function () {
+              $("#Errordiv").addClass("slide-out");
+              $("#Errordiv").removeClass("slide-in");
+              setTimeout(function () {
+                $("#Errordiv").removeClass("slide-out");
+                $("#Errordiv").addClass("display");
+              }, 500);
+            }, 2000);
+          }
         }
-      });
+      } else {
+        $("#errortext").html("Please Write Your Email Down");
+        $("#Errordiv").removeClass("display");
+        setTimeout(function () {
+          $("#Errordiv").addClass("slide-out");
+          $("#Errordiv").removeClass("slide-in");
+          setTimeout(function () {
+            $("#Errordiv").removeClass("slide-out");
+            $("#Errordiv").addClass("display");
+          }, 500);
+        }, 2000);
+      }
     });
   } else if (index_code == 1) {
     $.getJSON("data.json", function (data) {
       const existingemail = data.users.map((user) => user.mail);
       var codemail = $("#Emailplace").val();
       if (existingemail.includes(codemail)) {
-        alert("Check Your Mail");
+        $("#errortext").html("Check Your Mail");
+        $("#Errordiv").removeClass("display");
+        setTimeout(function () {
+          $("#Errordiv").addClass("slide-out");
+          $("#Errordiv").removeClass("slide-in");
+          setTimeout(function () {
+            $("#Errordiv").removeClass("slide-out");
+            $("#Errordiv").addClass("display");
+          }, 500);
+        }, 2000);
         setTimeout(function () {
           $("#Emailplace").val("");
           $("#Emailplace").prop("placeholder", "Input Your Code Here");
@@ -82,9 +118,27 @@ $("#button").click(function (event) {
           forgot_mail = codemail;
         }, 10);
       } else if ($("#Emailplace").val() == "") {
-        alert("Input Your Email");
+        $("#errortext").html("Input Your Email");
+        $("#Errordiv").removeClass("display");
+        setTimeout(function () {
+          $("#Errordiv").addClass("slide-out");
+          $("#Errordiv").removeClass("slide-in");
+          setTimeout(function () {
+            $("#Errordiv").removeClass("slide-out");
+            $("#Errordiv").addClass("display");
+          }, 500);
+        }, 2000);
       } else {
-        alert("Wrong email");
+        $("#errortext").html("Wrong Email");
+        $("#Errordiv").removeClass("display");
+        setTimeout(function () {
+          $("#Errordiv").addClass("slide-out");
+          $("#Errordiv").removeClass("slide-in");
+          setTimeout(function () {
+            $("#Errordiv").removeClass("slide-out");
+            $("#Errordiv").addClass("display");
+          }, 500);
+        }, 2000);
       }
     });
   } else if (index_code == 2) {
@@ -99,7 +153,16 @@ $("#button").click(function (event) {
           $("#Emailplace").val("");
           index_code = 3;
         } else if (index_code == 2) {
-          alert("Invalid Code Please Check Again");
+          $("#errortext").html("Invalid Code");
+          $("#Errordiv").removeClass("display");
+          setTimeout(function () {
+            $("#Errordiv").addClass("slide-out");
+            $("#Errordiv").removeClass("slide-in");
+            setTimeout(function () {
+              $("#Errordiv").removeClass("slide-out");
+              $("#Errordiv").addClass("display");
+            }, 500);
+          }, 2000);
         } else if (index_code == 3) {
           event.preventDefault();
           var val = $("#Emailplace").val();
@@ -137,7 +200,7 @@ $("#button").click(function (event) {
 });
 
 $("#second_div").click(function () {
-  if (index_reg == 0) {
+  if (index_reg == 0 && index_code == 0 && index_login == 0) {
     index_reg = 1;
     index_login = 1;
     $("#passplace").after(
@@ -145,5 +208,50 @@ $("#second_div").click(function () {
     );
     $("#forgot").html("Go back?");
     $("#button").html("Sign Up");
+  } else if (index_reg == 0 && index_code == 1) {
+    $("#forgot").html("Go back?");
+    $("#Emailplace").prop("placeholder", "Email");
+    $("#requirements").remove();
+    $("#br1").after(
+      '<br id="br2"/>\n<input id="passplace"type="password"minlength="5"placeholder="Password"required/>\n<br id="br3"/>'
+    );
+    $("#passplace").after(
+      '<input id="passplace2" type="password" minlength="5" placeholder="Enter Your Password Again" required/>'
+    );
+    $("#forgot").html("Go back?");
+    $("#button").html("Sign up");
+    index_code = 0;
+    index_reg = 1;
+    index_login = 1;
+  } else if (index_reg == 0 && index_code == 2) {
+    $("#forgot").html("Go back?");
+    $("#Emailplace").prop("placeholder", "Email");
+    $("#requirements").remove();
+    $("#br1").after(
+      '<br id="br2"/>\n<input id="passplace"type="password"minlength="5"placeholder="Password"required/>\n<br id="br3"/>'
+    );
+    $("#passplace").after(
+      '<input id="passplace2" type="password" minlength="5" placeholder="Enter Your Password Again" required/>'
+    );
+    $("#forgot").html("Go back?");
+    $("#button").html("Sign up");
+    index_code = 0;
+    index_reg = 1;
+    index_login = 1;
+  } else if (index_reg == 0 && index_code == 3) {
+    $("#forgot").html("Go back?");
+    $("#Emailplace").prop("placeholder", "Email");
+    $("#requirements").remove();
+    $("#br1").after(
+      '<br id="br2"/>\n<input id="passplace"type="password"minlength="5"placeholder="Password"required/>\n<br id="br3"/>'
+    );
+    $("#passplace").after(
+      '<input id="passplace2" type="password" minlength="5" placeholder="Enter Your Password Again" required/>'
+    );
+    $("#forgot").html("Go back?");
+    $("#button").html("Sign up");
+    index_code = 0;
+    index_reg = 1;
+    index_login = 1;
   }
 });
