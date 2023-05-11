6f8ce36e89fdac398953e4e10487819f79ce5782
projectgiorgibregadze <bregadzeg8@gmail.com>
2023-05-11 19:04:36 +0400

    main page 5/9%

--- Main-page/main.js
+++ Main-page/main.js
@@ -1,1 +1,27 @@
-// soon
+function resizewind() {
+  if ($(window).width() <= 730) {
+    $("#homenav,#tutornav,#menunav,#recipenav,#contactnav,#subnav").html("");
+    $("#Title").addClass(".textalign");
+    $("#homenav").append('<i class="fa-solid fa-house"></i>');
+    $("#tutornav").append('<i class="fa-solid fa-circle-question"></i>');
+    $("#menunav").append('<i class="fa-sharp fa-solid fa-list"></i>');
+    $("#recipenav").append('<i class="fa-solid fa-registered"></i>');
+    $("#contactnav").append('<i class="fa-solid fa-address-book"></i>');
+    $("#subnav")
+      .append('<i class="fa-solid fa-sack-dollar"></i>')
+      .removeClass("navlasta");
+    $("#rightpartcontent").hide();
+  } else {
+    $("#homenav").html("Home");
+    $("#tutornav").html("How to Order");
+    $("#menunav").html("Our Menu");
+    $("#recipenav").html("Our Recipes");
+    $("#contactnav").html("Contact Us");
+    $("#subnav").html("Subscribe").addClass("navlasta");
+    $("#rightpartcontent").show();
+  }
+}
+resizewind();
+$(window).resize(function () {
+  resizewind();
+});
