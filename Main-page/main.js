9c16d18b59b058aca44334417d45a1a7d576bdf2
projectgiorgibregadze <bregadzeg8@gmail.com>
2023-05-28 16:27:38 +0400

    ..

--- Main-page/main.js
+++ Main-page/main.js
@@ -225,11 +225,11 @@ $('#firstpicdot').click(
       )
       $('#comnumb').click(
         function () {
-          window.location.href='/Main_page/add/comment.html'
+          window.location.href='/Main-page/add/comment.html'
         }
       )
       $('#loldad').click(
         function () {
-          window.location.href='/Main_page/add/comments/main.html'
+          window.location.href='/Main-page/add/comments/main.html'
         }
       )
